a809f2e710048c4b126aa473bef7321c186719ce
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2021-01-17 00:38:02 -0300

    Craete a collection View Cell

---
+++ CollectionView/NumberCell.swift
@@ -0,0 +1,17 @@
+//
+//  NumberCell.swift
+//  CollectionView
+//
+//  Created by Henry AT on 17-01-21.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+    
+  static let reuseIdentifier = String(describing: NumberCell.self)
+  
+  
+  @IBOutlet weak var label: UILabel!
+  
+}
