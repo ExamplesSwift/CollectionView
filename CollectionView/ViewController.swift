64277777d7ba7964b2c39a5656406262610aa4c0
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2021-01-17 08:13:37 -0300

    Cofig to show y 2 colums

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
   
   func configureLayout() -> UICollectionViewCompositionalLayout {
     
-    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+    let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
     let item = NSCollectionLayoutItem(layoutSize: itemSize)
     
     let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44.0))
